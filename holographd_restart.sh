3baf3f954329d6ad0014a2c8bab76519996f003d
Artur <arthur.escada.20@gmail.com>
2023-07-09 12:14:39 +0300

    Update holographd_restart.sh

--- holographd_restart.sh
+++ holographd_restart.sh
@@ -1,13 +1,33 @@
 while true
 do
-        printf "Start"
         date=$(date +"%H:%M")
         echo Last Update: ${date}
         for((m=0; m<1; m++))
         do
         sudo systemctl restart holographd
-                printf "wait 1m"
-                sleep 1m
+                printf "wait 3 hours;"
+                printf "\n"
+                sleep 60m
+                printf "wait 2 hours;"
+                printf "\n"
+                sleep 60m
+                printf "wait 1 hours;"
+                printf "\n"
+                sleep 30m
+                printf "wait 30 minutes;"
+                printf "\n"
+                sleep 15m
+                printf "wait 15 minutes;"
+                printf "\n"
+                sleep 5m
+                printf "wait 10 minutes;"
+                printf "\n"
+                sleep 5m
+                printf "wait 5 minutes;"
+                printf "\n"
+                sleep 2m
+                printf "wait 3 minutes;"
+                sleep 3m
         done
         printf "\n"
 done
