465bbb022022ef3ef317fe1e725c3efee88c3792
Artur <arthur.escada.20@gmail.com>
2023-07-09 12:22:46 +0300

    Update holographd_restart.sh

--- holographd_restart.sh
+++ holographd_restart.sh
@@ -5,31 +5,31 @@ do
         for((m=0; m<1; m++))
         do
         sudo systemctl restart holographd
-                printf "restart after 3 hours"
+                printf "Restart after 3 hours"
                 printf "\n"
                 sleep 60m
-                printf "restart after 2 hours"
+                printf "Restart after 2 hours"
                 printf "\n"
                 sleep 60m
-                printf "restart after 1 hours"
+                printf "Restart after 1 hours"
                 printf "\n"
                 sleep 30m
-                printf "restart after 30 minutes"
+                printf "Restart after 30 minutes"
                 printf "\n"
                 sleep 15m
-                printf "restart after 15 minutes"
+                printf "Restart after 15 minutes"
                 printf "\n"
                 sleep 5m
-                printf "restart after 10 minutes"
+                printf "Restart after 10 minutes"
                 printf "\n"
                 sleep 5m
-                printf "restart after 5 minutes"
+                printf "Restart after 5 minutes"
                 printf "\n"
                 sleep 2m
-                printf "restart after 3 minutes"
+                printf "Restart after 3 minutes"
                 printf "\n"
                 sleep 3m
         done
-        printf "restart holographd"
+        printf "Restart holographd"
         printf "\n"
 done
